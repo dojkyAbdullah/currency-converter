88151e41b7f373d3740ab91b5a05db4969c48f61
Abdullah Dojky <dojkyabdullah@gmail.com>
2025-04-24 23:30:56 +0500

    Fix import case sensitivity for Vercel

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import Check from './Components/check.jsx'
+import Check from './Components/Check.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
